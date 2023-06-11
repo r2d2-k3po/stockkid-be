a1da6f29eb38f69d1bdb3af53f82cf2dddcc9394
r2d2-k3po <r2d2factory3@gmail.com>
2023-06-11 11:46:00 +0900

    spring data JPA, PostgreSQL, application.properties: postgresql settings

--- src/main/java/net/stockkid/stockkidbe/StockkidBeApplication.java
+++ src/main/java/net/stockkid/stockkidbe/StockkidBeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StockkidBeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StockkidBeApplication.class, args);
 	}
