feb6b9a8697a9e7d4529c5c7eaa16143bda1cef3
r2d2-k3po <r2d2factory3@gmail.com>
2023-10-31 21:36:57 +0900

    Board: @Index, BoardCategory

--- src/main/java/net/stockkid/stockkidbe/entity/Board.java
+++ src/main/java/net/stockkid/stockkidbe/entity/Board.java
@@ -10,6 +10,13 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(indexes = {
+        @Index(columnList = "memberId"),
+        @Index(columnList = "rootId"),
+        @Index(columnList = "tag1"),
+        @Index(columnList = "tag2"),
+        @Index(columnList = "tag3")
+})
 public class Board extends BaseEntity {
 
     @Id
@@ -19,24 +26,42 @@ public class Board extends BaseEntity {
     @NotNull
     private @Setter Long memberId;
 
-    @Column
+    @NotNull
     private @Setter Long rootId;
 
     @Column
     private @Setter Long parentId;
 
+    @Column
+    @Enumerated(EnumType.STRING)
+    private @Setter BoardCategory boardCategory;
+
     @NotNull
     private @Setter String nickname;
 
-    @NotNull
+    @Column
     private @Setter String title;
 
     @Lob
-    @Column
+    @NotNull
     @ToString.Exclude
     private @Setter String content;
 
     @Column
+    private @Setter String tag1;
+
+    @Column
+    private @Setter String tag2;
+
+    @Column
+    private @Setter String tag3;
+
+    @Column
+    private @Setter int readCount;
+
+    @Column
     private @Setter int replyCount;
 
+    @NotNull
+    private @Setter int likeCount;
 }
---
+++ src/main/java/net/stockkid/stockkidbe/entity/BoardCategory.java
@@ -0,0 +1,5 @@
+package net.stockkid.stockkidbe.entity;
+
+public enum BoardCategory {
+    STOCK, LIFE, QA, NOTICE
+}
